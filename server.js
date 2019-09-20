2b5158ab790d921aa3007279e87d3472b52652b5
Joe Klause <joeklause@gmail.com>
2019-09-20 12:01:27 -0700

    dealt with request in server

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const url = require('url');
 
 function start(route, handle) {
   http.createServer((request, response) => {
+    let postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
 
-    route(handle, pathname, response);
+    request.setEncoding('utf8');
+
+    request.addListener('data', (postDataChunk) => {
+      postData += postDataChunk;
+      console.log(`Received POST data chunk ${postDataChunk}`);
+    });
     
+    request.addListener('end', () => {
+      route(handle, pathname, response, postData);
+    });
   }).listen(8888);
 
-console.log('Server has started');
-
+  console.log('Server has started');
 }
 
 exports.start = start;
