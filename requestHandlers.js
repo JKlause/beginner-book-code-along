ed8b1a610e362f5519fcb50ec50158d3f30955ee
Joe Klause <joeklause@gmail.com>
2019-09-19 17:01:18 -0700

    change for non-blocking issue

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,13 @@
+const exec = require('child_process').exec;
+
 function start() {
   console.log('Request handler "start" was called.');
-
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while(new Date().getTime() < startTime + milliSeconds);
-  }
-sleep(10000);
-  return 'Hello Start';
+  const content = 'empty';
+  
+  exec('ls -lah', function (error, stdout, stderr) {
+    content = stdout;
+  })
+  return content;
 }
 
 function upload() {
