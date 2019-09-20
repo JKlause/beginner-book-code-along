08c22ef0caaed06bff289bc21fa74bf9bc26502a
Joe Klause <joeklause@gmail.com>
2019-09-20 13:25:07 -0700

    finished project!

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 const querystring = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
 function start(response) {
   console.log('Request handler "start" was called.');
@@ -23,12 +24,25 @@ function start(response) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log('Request handler "upload" was called.');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-    postData = querystring(postData).text;
-    response.write(`You've sent: ${postData}`);
+
+  const form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, (error, fields, files) => {
+    console.log('parsing done');
+
+    fs. rename(files.upload.path, '/tmp/test.png', (error) => {
+      if(error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image:<br/>');
+    response.write('<img src="/show" />');
     response.end();
+  });
 }
 
 function show(response) {
--- server.js
+++ server.js
@@ -5,8 +5,7 @@ function start(route, handle) {
   http.createServer((request, response) => {
     const pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
-    route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   }).listen(8888);
 
   console.log('Server has started');
